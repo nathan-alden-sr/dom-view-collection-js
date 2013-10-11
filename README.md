e2bad2b0cc087aaa77beea8a2219d505c1180119
Nathan Alden, Sr <me@nathanalden.com>
2013-10-10 20:54:18 -0500

    Added several new events Now defining DomViewCollection prototype for efficiency DomViewCollections are now created with 'new' keyword Initial elements may now be provided in options parameter

--- README.md
+++ README.md
@@ -5,10 +5,13 @@ Table of contents:
  
 ## Version history
 
-#### 0.9.1
+#### 0.9.2
 
 * Released 2013-10-10
 * Added getMapped function to retrieve collection elements as a single jQuery object
+* Added several new events
+* DomViewCollections are now created with 'new' keyword; added underlying prototype for efficiency
+* Initial elements may now be provided in options parameter
 
 #### 0.9.0
 
--- domviewcollection.js
+++ domviewcollection.js
@@ -1,4 +1,4 @@
-// DOMViewCollection.js 0.9.1
+// DOMViewCollection.js 0.9.2
 
 // Created by Nathan Alden, Sr.
 // http://projects.nathanalden.com
@@ -10,97 +10,135 @@
 	window.DomViewCollection = function (options) {
 		options = $.extend({}, window.DomViewCollection.defaults, options);
 
-		var elements = [];
-		var collection = {
-			get: function () {
-				return elements.slice(0);
-			},
-			getMapped: function () {
-				return $(this.get()).map(function () { return this.toArray(); });
-			},
-			count: function () {
-				return elements.length;
-			},
-			insert: function (index, element) {
-				if (index < 0 || index > elements.length) {
-					return undefined;
-				}
+		this._elements = [];
+		this.options = options;
 
-				var filling = elements.length === 0;
+		options.init.call(this);
 
-				elements.splice(index, 0, element);
-
-				this.options.added.call(this, index, element);
-				if (filling) {
-					this.options.filled.call(this, elements.length);
-				}
-
-				return element;
-			},
-			add: function (element) {
-				return this.insert(elements.length, element);
-			},
-			addMany: function (elements) {
-				if (!$.isArray(elements)) {
-					throw "Must provide an array";
-				}
-				for (var i = 0; i < elements.length; i++) {
-					this.add(elements[i]);
-				}
-			},
-			removeAtIndex: function (index) {
-				if (index < 0 || index >= elements.length) {
-					return undefined;
-				}
-
-				var emptying = elements.length === 1;
-				var removedElement = elements.splice(index, 1);
-
-				this.options.removed.call(this, index, removedElement[0], elements.length);
-				if (emptying) {
-					this.options.emptied.call(this);
-				}
-
-				return removedElement;
-			},
-			remove: function (element) {
-				return this.removeAtIndex(elements.indexOf(element));
-			},
-			removeMany: function (elements) {
-				if (!$.isArray(elements)) {
-					throw "Must provide an array";
-				}
-				for (var i = 0; i < elements.length; i++) {
-					this.remove(elements[i]);
-				}
-			},
-			clear: function () {
-				var emptying = elements.length > 0;
-				var removedElements = this.get();
-
-				elements.length = 0;
-
-				this.options.cleared.call(this);
-				if (emptying) {
-					this.options.emptied.call(this);
-				}
-
-				return removedElements;
-			},
-			options: options
-		};
-
-		options.init.call(collection);
-
-		return collection;
+		var elementsToAdd = options.elements === undefined || options.elements === null ? [] : options.elements;
+		
+		for (var i = 0; i < elementsToAdd.length; i++) {
+			this.add(elementsToAdd[i], this);
+		}
 	};
 
+	$.extend(window.DomViewCollection.prototype, {
+		get: function () {
+			return this._elements.slice(0);
+		},
+		getMapped: function () {
+			return $(this.get()).map(function () { return this instanceof jQuery ? this.toArray() : this; });
+		},
+		count: function () {
+			return this._elements.length;
+		},
+		insert: function (index, element, context) {
+			if (index < 0 || index > this._elements.length) {
+				return undefined;
+			}
+
+			var filling = this._elements.length === 0;
+
+			if (filling && this.options.filling.call(this, context) === false) {
+				return undefined;
+			}
+			if (this.options.adding.call(this, element, index, this._elements.length, context) === false) {
+				return undefined;
+			}
+
+			var projectedElement = this.options.project.call(this, element);
+
+			element = projectedElement === undefined || projectedElement === null ? element : projectedElement;
+
+			this._elements.splice(index, 0, element);
+
+			this.options.added.call(this, element, index, this._elements.length, context);
+			if (filling) {
+				this.options.filled.call(this, context);
+			}
+
+			return element;
+		},
+		add: function (element, context) {
+			return this.insert(this._elements.length, element, context);
+		},
+		addMany: function (elements, context) {
+			if (!$.isArray(elements)) {
+				throw "Must provide an array";
+			}
+			for (var i = 0; i < elements.length; i++) {
+				this.add(elements[i], context);
+			}
+		},
+		removeAtIndex: function (index, context) {
+			if (index < 0 || index >= this._elements.length) {
+				return undefined;
+			}
+
+			var emptying = this._elements.length === 1;
+
+			if (emptying && this.options.emptying.call(this, context) === false) {
+				return undefined;
+			}
+			if (this.options.removing.call(this, this._elements[index], index, this._elements.length, context) === false) {
+				return undefined;
+			}
+
+			var removedElement = this._elements.splice(index, 1);
+
+			this.options.removed.call(this, removedElement[0], index, this._elements.length, context);
+			if (emptying) {
+				this.options.emptied.call(this, context);
+			}
+
+			return removedElement;
+		},
+		remove: function (element, context) {
+			return this.removeAtIndex(this._elements.indexOf(element), context);
+		},
+		removeMany: function (elements, context) {
+			if (!$.isArray(elements)) {
+				throw "Must provide an array";
+			}
+			for (var i = 0; i < elements.length; i++) {
+				this.remove(elements[i], context);
+			}
+		},
+		clear: function (context) {
+			var emptying = this._elements.length > 0;
+			var removedElements = this.get();
+
+			if (emptying && this.options.emptying.call(this, this._elements.length, context) === false) {
+				return undefined;
+			}
+			if (this.options.clearing.call(this, this._elements.length, context) === false) {
+				return undefined;
+			}
+
+			this._elements.length = 0;
+
+			this.options.cleared.call(this, context);
+			if (emptying) {
+				this.options.emptied.call(this, context);
+			}
+
+			return removedElements;
+		}
+	});
+
 	window.DomViewCollection.defaults = {
+		elements: [],
 		init: $.noop,
+		adding: $.noop,
+		project: $.noop,
 		added: $.noop,
+		removing: $.noop,
 		removed: $.noop,
+		clearing: $.noop,
 		cleared: $.noop,
+		filling: $.noop,
 		filled: $.noop,
+		emptying: $.noop,
 		emptied: $.noop
 	};
 })(jQuery);
